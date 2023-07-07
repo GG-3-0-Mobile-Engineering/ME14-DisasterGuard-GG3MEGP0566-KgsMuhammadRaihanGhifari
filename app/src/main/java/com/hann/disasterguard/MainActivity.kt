b64528737606f4ee5fd16d84eae1b4661af626ca
Kgs Muhammad Raihan Ghifari <raihan.ghifari1603@gmail.com>
2023-07-08 00:10:56 +0700

    initial commit creating project

---
+++ app/src/main/java/com/hann/disasterguard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hann.disasterguard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
