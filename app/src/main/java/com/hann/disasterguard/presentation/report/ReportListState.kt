beab201f847663a51fb8595e8e9549c164bd7793
Kgs Muhammad Raihan Ghifari <raihan.ghifari1603@gmail.com>
2023-08-09 23:14:39 +0700

    refactor(report): deleting report fragment

--- app/src/main/java/com/hann/disasterguard/presentation/report/ReportListState.kt
+++
@@ -1,9 +0,0 @@
-package com.hann.disasterguard.presentation.report
-
-import com.hann.disasterguard.coreapp.domain.model.GeometryReport
-
-data class ReportListState (
-    val isLoading : Boolean = false,
-    val report: List<GeometryReport> = emptyList(),
-    val error: String = ""
-)
